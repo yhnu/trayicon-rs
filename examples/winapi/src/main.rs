3ba6cdb82db1fbe8b6c0864a64b5681d3a74b2fe
yhnu <buutuud@gmail.com>
2021-08-19 14:30:54 +0800

    add change host

--- examples/winapi/src/main.rs
+++ examples/winapi/src/main.rs
@@ -1,6 +1,12 @@
 use core::mem::MaybeUninit;
 use trayicon::*;
 use winapi::um::winuser;
+mod file;
+mod parse;
+mod req;
+use file::*;
+use parse::Hosts;
+use req::*;
 
 fn main() {
     #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -35,23 +41,23 @@ fn main() {
         .menu(
             MenuBuilder::new()
                 .item("Item 3 Replace Menu 👍", Events::Item3)
-                .item("Item 2 Change Icon Green", Events::Item2)
-                .item("Item 1 Change Icon Red", Events::Item1)
-                .separator()
-                .checkable("This is checkable", true, Events::CheckItem1)
-                .submenu(
-                    "Sub Menu",
-                    MenuBuilder::new()
-                        .item("Sub item 1", Events::SubItem1)
-                        .item("Sub Item 2", Events::SubItem2)
-                        .item("Sub Item 3", Events::SubItem3),
-                )
-                .with(MenuItem::Item {
-                    name: "Item Disabled".into(),
-                    disabled: true, // Disabled entry example
-                    id: Events::Item4,
-                    icon: None,
-                })
+                //.item("Item 2 Change Icon Green", Events::Item2)
+                //.item("Item 1 Change Icon Red", Events::Item1)
+                //.separator()
+                //.checkable("This is checkable", true, Events::CheckItem1)
+                // .submenu(
+                //     "Sub Menu",
+                //     MenuBuilder::new()
+                //         .item("Sub item 1", Events::SubItem1)
+                //         .item("Sub Item 2", Events::SubItem2)
+                //         .item("Sub Item 3", Events::SubItem3),
+                // )
+                // .with(MenuItem::Item {
+                //     name: "Item Disabled".into(),
+                //     disabled: true, // Disabled entry example
+                //     id: Events::Item4,
+                //     icon: None,
+                // })
                 .separator()
                 .item("E&xit", Events::Exit),
         )
@@ -68,6 +74,7 @@ fn main() {
             }
             Events::Exit => {
                 println!("Please exit");
+                std::process::exit(0);
             }
             Events::Item1 => {
                 tray_icon.set_icon(&second_icon).unwrap();
@@ -76,13 +83,14 @@ fn main() {
                 tray_icon.set_icon(&first_icon).unwrap();
             }
             Events::Item3 => {
-                tray_icon
-                    .set_menu(
-                        &MenuBuilder::new()
-                            .item("New menu item", Events::Item1)
-                            .item("Exit", Events::Exit),
-                    )
-                    .unwrap();
+                if let Some(hosts) = fetch() {
+                    let hosts = Hosts::from(hosts);
+
+                    match mod_hosts_file(hosts.to_string()) {
+                        Ok(_) => println!("Success!"),
+                        Err(err) => eprintln!("Error: {}", err),
+                    }
+                }
             }
             e => {
                 println!("{:?}", e);
